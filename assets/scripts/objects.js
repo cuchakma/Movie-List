fe07150940dc85254187e6d0b5bf122eff400be9
cuchakma <greendaycu20@gmail.com>
2021-07-11 23:11:25 +0600

    added search feature

--- assets/scripts/objects.js
+++ assets/scripts/objects.js
@@ -5,15 +5,23 @@ const addMovieBtn = document.getElementById('add-movie-btn');
 const movie = [];
 
 //Render Movie
-const renderMovies = () => {
+const renderMovies = (filterValue = '') => {
     const movieList = document.getElementById('movie-list');
     if( movie.length > 0 ) {
         movieList.classList.add('visible');
     }
     movieList.innerHTML = '';
-    movie.forEach((movie)=>{
+    const filteredMovies = !filterValue ? movie : movie.filter(movie => movie.info.title.includes(filterValue));
+
+    filteredMovies.forEach((movie)=>{
         const movieEl = document.createElement('li');
-        movieEl.textContent = movie.info.title;
+        let text = movie.info.title + ' - ';
+        for (const key in movie.info) {
+            if( key != 'title' ) {
+                text = text + `${key}:${movie.info[key]}`;
+            }
+        }
+        movieEl.textContent = text;
         movieList.append(movieEl);
     });
 };
@@ -41,5 +49,9 @@ const addMovieHandler = () => {
     movie.push(newMovie);
     renderMovies();
 };
-
+const searchMovieHandler = () => {
+    const searchKeyword = document.getElementById('filter-title').value;
+    renderMovies(searchKeyword);
+}
 addMovieBtn.addEventListener('click', addMovieHandler);
+searchBtn.addEventListener('click',searchMovieHandler);
